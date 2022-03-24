702911d48e35d8a39440c9fb9276eeb2588e2227
byamane <byamane21@gmail.com>
2022-03-24 08:42:30 -0500

    construct project model

--- models/profile.js
+++ models/profile.js
@@ -1,8 +1,12 @@
-import mongoose from 'mongoose'
+import mongoose, { Schema } from 'mongoose'
+
+const Schema = mongoose.Schema
 
 const profileSchema = new mongoose.Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
   name: String,
+  avatar: String,
+  projects: [{type: Schema.Types.ObjectId, ref: "Project"}]
 },{
     timestamps: true,
 })
---
+++ models/project.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const projectSchema = new Schema({
+  repoId: String,
+  repoName: String,
+  repoCommits: [String],
+  goals: [String],
+  progress: Number,
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"}
+}, {
+  timestamps: true,
+})
+
+const Project = mongoose.model("Project", projectSchema)
+
+export {
+  Project
+}
